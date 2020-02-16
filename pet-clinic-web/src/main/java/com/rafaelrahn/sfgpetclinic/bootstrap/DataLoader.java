03cec348070826b65f01f9b24f9e396245671059
Rafael Rahn <rafael.rahn@live.com>
2020-02-16 16:03:42 +0100

    Generate auto id AbstractMap

--- pet-clinic-web/src/main/java/com/rafaelrahn/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rafaelrahn/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Maria");
         owner1.setLastName("Lura");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Matilda");
         owner2.setLastName("Lontra");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Leoncio");
         vet1.setLastName("Lupresto");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Carlos");
         vet2.setLastName("Caramujo");
 
