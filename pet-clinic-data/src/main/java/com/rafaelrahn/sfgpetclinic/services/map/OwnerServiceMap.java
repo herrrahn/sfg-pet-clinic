2e4e04c2e45cd27a42cbcfe794ddfe244b2b70da
Rafael Rahn <rafael.rahn@live.com>
2020-04-13 11:09:08 +0200

    added Pet and Contact info to owner on startup

--- pet-clinic-data/src/main/java/com/rafaelrahn/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/rafaelrahn/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,13 +1,24 @@
 package com.rafaelrahn.sfgpetclinic.services.map;
 
 import com.rafaelrahn.sfgpetclinic.model.Owner;
+import com.rafaelrahn.sfgpetclinic.model.Pet;
 import com.rafaelrahn.sfgpetclinic.services.OwnerService;
+import com.rafaelrahn.sfgpetclinic.services.PetService;
+import com.rafaelrahn.sfgpetclinic.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long>  implements OwnerService {
+
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public OwnerServiceMap(PetService petService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -31,7 +42,27 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long>  implements
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach( pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet type is required.");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/com/rafaelrahn/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/rafaelrahn/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.rafaelrahn.sfgpetclinic.bootstrap;
 
 import com.rafaelrahn.sfgpetclinic.model.Owner;
+import com.rafaelrahn.sfgpetclinic.model.Pet;
 import com.rafaelrahn.sfgpetclinic.model.PetType;
 import com.rafaelrahn.sfgpetclinic.model.Vet;
 import com.rafaelrahn.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.rafaelrahn.sfgpetclinic.services.PetTypeService;
 import com.rafaelrahn.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -23,7 +26,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         PetType petType = new PetType();
         petType.setName("Cat");
@@ -37,12 +40,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Maria");
         owner1.setLastName("Lura");
+        owner1.setAddress("Rua Pirulito Rosa 123");
+        owner1.setCity("Blumenau");
+        owner1.setCity("123 3212 344");
+
+        Pet mariasPet = new Pet();
+        mariasPet.setPetType(savePetTypeCat);
+        mariasPet.setName("Laura");
+        mariasPet.setOwner(owner1);
+        mariasPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(mariasPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Matilda");
         owner2.setLastName("Lontra");
+        owner2.setAddress("Rua Chambinho Puro 9");
+        owner2.setCity("Pomerode");
+        owner2.setCity("987 37624 344");
+
+        Pet matildasPet = new Pet();
+        matildasPet.setPetType(savePetTypeDog);
+        matildasPet.setName("Otavio");
+        matildasPet.setOwner(owner2);
+        matildasPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(matildasPet);
 
         ownerService.save(owner2);
 
